c7315a98e28f1221e0dc026398097993d969328b
Your Name <[ashutosh.singh3889@gmail.com]>
2026-08-08 07:15:21 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello app");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from inside the Docker container!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
